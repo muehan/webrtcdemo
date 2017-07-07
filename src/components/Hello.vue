e47a474fb79f037795135eb30d275ba15a3125cf
andre <tdc@gmx.ch>
2017-07-07 14:24:11 +0200

    initial commit

---
+++ src/components/Hello.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    
+    </ul>
+  </div>
+</template>
+
+<script>
+import peer from 'peerjs/dist/peer.js';
+
+export default {
+  name: 'hello',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
